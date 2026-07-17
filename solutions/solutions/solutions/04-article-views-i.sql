87dc8f3650d79edc50eb73a221d6e65bfe2ebca1
Jyoti1918 <jyotikumari1234561899@gmail.com>
2026-07-17 13:27:52 +0530

    Delete solutions/solutions/solutions directory

--- solutions/solutions/solutions/04-article-views-i.sql
+++
@@ -1,8 +0,0 @@
--- Problem: Article Views I
--- Difficulty: Easy
--- Concepts: SELECT, DISTINCT, AS, WHERE, ORDER BY
-
-SELECT DISTINCT author_id AS id
-FROM Views
-WHERE author_id = viewer_id
-ORDER BY id ASC;
