486b0a4d01101bd78fbca7682bb2774020fbe5a3
Jyoti1918 <jyotikumari1234561899@gmail.com>
2026-08-05 01:09:20 +0530

    Implement rising temperature query
    Add SQL query to find days with rising temperatures.

--- solutions/09-rising-temperature.sql
+++ solutions/09-rising-temperature.sql
@@ -1,1 +1,9 @@
+-- Problem: Rising Temperature
+-- Difficulty: Easy
+-- Concepts: SELF JOIN, DATEDIFF, WHERE, aliases
 
+SELECT w1.id
+FROM Weather w1
+JOIN Weather w2
+    ON DATEDIFF(w1.recordDate, w2.recordDate) = 1
+WHERE w1.temperature > w2.temperature;
